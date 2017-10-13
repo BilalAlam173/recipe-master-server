dd5cc5d31b6d38d52c21d63c97a0bd2667d8ea89
Muhammad Bilal Alam <bilalalam173@gmail.com>
2017-10-13 03:11:25 -0700

    test

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+app.get('/test',function(req,res){
+
+    res.a="testing";
+
+});
+
 app.listen(process.env.PORT || 8090, function () {
     console.log('Example app listening on port 8090!')
 });
