8b170a4733693810da50d02d32ce0e195aad5601
Muhammad Bilal Alam <bilalalam173@gmail.com>
2017-10-13 03:16:05 -0700

    test api

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.get('/test',function(req,res){
 
-    res.a="testing";
+    res.send("testing");
 
 });
-
+console.log("test");
 app.listen(process.env.PORT || 8090, function () {
     console.log('Example app listening on port 8090!')
 });
